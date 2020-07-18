2a98a0b861e380ff1a820cf47a9a07a6dbe9363b
Nadav Schwartz <nadavschwartz58@gmail.com>
2020-07-18 15:52:44 +0000

    Automatically backed up by Learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -41,4 +41,10 @@ class InteractiveRecord
     end
     values.join(", ")
   end
+  
+
+  def self.find_by_name(name)
+  sql = "SELECT * FROM #{self.table_name} WHERE name = ?"
+  DB[:conn].execute(sql, name)
+  end
 end
